779a42741290ceedb8b00694d8a7538ae50f5290
MarceloMartins-create <marcelocunha@estudante.ufscar.br>
2026-07-01 14:45:44 -0300

    Update arvoreB.c

--- arvoreB.c
+++ arvoreB.c
@@ -19,7 +19,7 @@ typedef struct arvoreB{
 }arvoreB;
 
 
-FILE* abrir_arquivo(int *rrn_raiz, int *num_nos){
+FILE* abrir_arquivo(arvoreB *a1){
     FILE* fp = fopen("indice_primario.dat", "rb+");
     
     if (fp == NULL){
@@ -29,15 +29,15 @@ FILE* abrir_arquivo(int *rrn_raiz, int *num_nos){
             exit(0);
         }
         
-        *rrn_raiz = -1;
-        *num_nos = 0;
-        fwrite(rrn_raiz, sizeof(int), 1, fp);
-        fwrite(num_nos, sizeof(int), 1, fp);
+        a1->rrn_raiz = -1;
+        a1->num_nos = 0;
+        fwrite(&a1->rrn_raiz, sizeof(int), 1, fp);
+        fwrite(&a1->num_nos, sizeof(int), 1, fp);
         fflush(fp);
     }
     rewind(fp);
-    fread(rrn_raiz, sizeof(int), 1, fp);
-    fread(num_nos, sizeof(int), 1, fp);    
+    fread(&a1->rrn_raiz, sizeof(int), 1, fp);
+    fread(&a1->num_nos, sizeof(int), 1, fp);    
 
     return fp;
 }
@@ -70,14 +70,79 @@ void printar_no(no *n1){
 }
 
 
+int escrever_no(FILE *fp, int rrn, no *n1, arvoreB *a1){
+    if (rrn < 0 || rrn > a1->num_nos){
+        printf("RRN INVALIDO");
+        return 0;
+    }
+    long offset = (2*sizeof(int))+(rrn*sizeof(no));
+    fseek(fp, offset, SEEK_SET);
+    
+    if (fwrite(n1, sizeof(no), 1, fp)!= 1){
+        printf("ERRO DE ESCRITA\n");
+        return 0;
+    }
+    
+    fflush(fp);
+    
+    printf("NO ESCRITO COM SUCESSO\n");
+    return 1;
+}
+
+int atualizar_cabecalho(FILE *fp, arvoreB *a1){
+    rewind(fp);
+    fwrite(&a1->rrn_raiz, sizeof(int), 1, fp);
+    fwrite(&a1->num_nos, sizeof(int), 1, fp);
+    fflush(fp);
+    return 1;
+}
+
+void inicializar_no(no *n1, int folha){
+    n1->is_folha = folha;
+    n1->num_chaves = 0;
+    for (int i = 0; i<ORDEM; i++){
+        if (i<ORDEM-1){
+            n1->chaves[i] = -1;
+            n1->rrn_dados[i] = -1;
+        }
+        n1->rrn_filhos[i] = -1;
+    }
+
+}
+
+int inserir_arvore_vazia(FILE *fp, arvoreB *a1, int chave, int rrn_dado){
+    no raiz;
+    inicializar_no(&raiz, 1);
+    
+    raiz.chaves[0] = chave;
+    raiz.rrn_dados[0] = rrn_dado;
+    raiz.num_chaves = 1;
+    
+    a1->rrn_raiz = 0;
+    a1->num_nos = 1;
+    escrever_no(fp, 0, &raiz, a1);
+    
+    atualizar_cabecalho(fp, a1);
+    return 1;
+}
 
 
+int inserir_com_espaco(no *n1, int chave, int rrn_dado){
+    if (n1->num_chaves == ORDEM - 1)
+        return 0;
 
+    int i = n1->num_chaves - 1;
 
+    while (i >= 0 && n1->chaves[i] > chave){
+        n1->chaves[i + 1] = n1->chaves[i];
+        n1->rrn_dados[i + 1] = n1->rrn_dados[i];
+        i--;
+    }
 
+    n1->chaves[i + 1] = chave;
+    n1->rrn_dados[i + 1] = rrn_dado;
+    n1->num_chaves++;
 
-
-int escrever_no(FILE *fp, int rrn, no *n){
     return 1;
 }
 
@@ -86,14 +151,66 @@ int escrever_no(FILE *fp, int rrn, no *n){
 
 
 
-int main(){
-    int rrn_raiz;
-    int num_nos;
-    FILE *fp = abrir_arquivo(&rrn_raiz, &num_nos);
 
+
+
+int inserir(FILE *fp, arvoreB *a1, int chave, int rrn_dado){
+    if (a1->rrn_raiz == -1){
+        return inserir_arvore_vazia(fp, a1, chave, rrn_dado);
+    }
+
+    no raiz;
+
+    ler_no(fp, a1->rrn_raiz, a1, &raiz);
+    if (raiz.is_folha){
+        if (inserir_com_espaco(&raiz, chave, rrn_dado)){
+            escrever_no(fp, a1->rrn_raiz, &raiz, a1);
+            return 1;
+        }
+        
+    }
+
+
+}
+
+
+int buscar_no_correto(FILE *fp,arvoreB *a1, int rrn_atual, int chave, no *n1, int *rrn_resultado){
+    no atual;
+    while(1){
+        ler_no(fp, rrn_atual, a1, &atual);
+
+        int i = 0;
+
+        while(i < atual.num_chaves && chave > atual.chaves[i])
+            i++;
+
+        if(i < atual.num_chaves && chave == atual.chaves[i]){
+            *n1 = atual;
+            *rrn_resultado = rrn_atual;
+            return 1;
+        }
+
+        if(atual.is_folha){
+            *n1 = atual;
+            *rrn_resultado = rrn_atual;
+            return 0;
+        }
+        rrn_atual = atual.rrn_filhos[i];
+    }
+}
+
+
+
+
+
+
+
+int main(){
     arvoreB a1;
-    a1.num_nos = num_nos;
-    a1.rrn_raiz = rrn_raiz;
+    FILE *fp = abrir_arquivo(&a1);
+    
+
+    
     
     return 0;
 }
