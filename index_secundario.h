350a4a702208536bc4dd912dce5feb67c2f1c195
FSLouredo <felipeslouredo1@gmail.com>
2026-07-12 13:04:52 -0300

    estruturação da index_sec

---
+++ index_secundario.h
@@ -0,0 +1,23 @@
+#ifndef INDEX_SECUNDARIO_H
+#define INDEX_SECUNDARIO_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "crud.h"
+
+typedef struct indiceSecundario {
+    char chave[30];
+    int primeiro_rrn;
+} indiceSecundario;
+
+typedef struct noListaInvertada {
+    int rrn_dado;     
+    int proximo_rrn;  
+} noListaInvertada;
+
+int buscar_chave_secundaria(FILE *fp_idx, const char *chave, IndiceSecundario *res, int *pos_idx);
+void inserir_indice_secundario(FILE *fp_idx, FILE *fp_list, const char *chave, int rrn_dado);
+void remover_indice_secundario(FILE *fp_idx, FILE *fp_list, const char *chave, int rrn_dado);
+void buscar_e_printar_por_chave_secundaria(FILE *fp_idx, FILE *fp_list, FILE *fp_dados, const char *chave);
+
+#endif
