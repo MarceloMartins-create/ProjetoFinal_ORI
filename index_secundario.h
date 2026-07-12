03733a44a668e7db7ee5dc9a3427fc82c5195d45
FSLouredo <felipeslouredo1@gmail.com>
2026-07-12 13:32:26 -0300

    arrumar nome do registro

--- index_secundario.h
+++ index_secundario.h
@@ -10,12 +10,12 @@ typedef struct indiceSecundario {
     int primeiro_rrn;
 } indiceSecundario;
 
-typedef struct noListaInvertada {
+typedef struct noListaInvertida {
     int rrn_dado;     
     int proximo_rrn;  
-} noListaInvertada;
+} noListaInvertida;
 
-int buscar_chave_secundaria(FILE *fp_idx, const char *chave, IndiceSecundario *res, int *pos_idx);
+int buscar_chave_secundaria(FILE *fp_idx, const char *chave, indiceSecundario *res, int *pos_idx);
 void inserir_indice_secundario(FILE *fp_idx, FILE *fp_list, const char *chave, int rrn_dado);
 void remover_indice_secundario(FILE *fp_idx, FILE *fp_list, const char *chave, int rrn_dado);
 void buscar_e_printar_por_chave_secundaria(FILE *fp_idx, FILE *fp_list, FILE *fp_dados, const char *chave);
