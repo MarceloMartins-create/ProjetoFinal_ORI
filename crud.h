8e9a384efd2ca15e1a00f9f7f6f8c199169f83e4
FSLouredo <felipeslouredo1@gmail.com>
2026-07-12 14:23:44 -0300

    integração com index_sec e lista_invertida

--- crud.h
+++ crud.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "arvoreB.h"
+
 typedef struct registro{
     int id;
     int proximo_rrn;
@@ -21,13 +22,10 @@ FILE* abrir_dados();
 int verificar_cabeca(FILE *fp_dados);
 int atualizar_cabeca(FILE *fp_dados, int rrn);
 registro ler_dados(int code);
-int adicionar_dados(FILE* fp_dados, FILE* fp_indice, arvoreB* a1);
+int adicionar_dados(FILE* fp_dados, FILE* fp_indice, arvoreB* a1, FILE *fp_idx_titulo, FILE *fp_list_titulo, FILE *fp_idx_genero, FILE *fp_list_genero);
 int printar_registros(FILE *fp_dados);
-int excluir_registro(FILE *fp_dados, FILE *fp_indice, arvoreB *a1);
-int atualizar_registro(FILE *fp_dados, FILE *fp_indice, arvoreB *a1);
+int excluir_registro(FILE *fp_dados, FILE *fp_indice, arvoreB *a1, FILE *fp_idx_titulo, FILE *fp_list_titulo, FILE *fp_idx_genero, FILE *fp_list_genero);
+int atualizar_registro(FILE *fp_dados, FILE *fp_indice, arvoreB *a1, FILE *fp_idx_titulo, FILE *fp_list_titulo, FILE *fp_idx_genero, FILE *fp_list_genero);
 int ler_registro(FILE *fp_dados, FILE *fp_indice, arvoreB *a1);
-
-
-
 
 #endif
